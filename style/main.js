1b6361142b5469889ec5fae6333d414fc80a36d4
schermann <120848089+nxpinhum5326@users.noreply.github.com>
2024-12-14 23:08:29 +0300

    Add files via upload

---
+++ style/main.js
@@ -0,0 +1,4 @@
+function toggleDetails(matchElement){
+    const details = matchElement.querySelector(".details");
+    details.style.display = details.style.display === "block" ? "none" : "block";
+}
